4e0a6cdc95be4bec50108b0668a5d41b5cc15279
Raju yadav <138615548+Raju-07@users.noreply.github.com>
2025-07-02 14:54:55 +0530

    updated one

--- ReDo Automate with Selenium.py
+++ ReDo Automate with Selenium.py
@@ -1,62 +1,59 @@
 # Required framework and library
-import pyautogui
+import time
 from tkinter import messagebox as msg
-"Trying to automate the task using selenium"
+#"Trying to automate the task using selenium"
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.edge.options import Options
+
 
 # Set up the WebDriver (Edge in this example)
 options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Suppress DevTools message
-driver = webdriver.Chrome(options=options)
-
+driver = webdriver.Edge(options=options)
 
 # Navigate to the result portal
 driver.get("https://result.mdu.ac.in/postexam/result.aspx")
 
-student_details = []   #List for storing the student details
+res_std_data = []
 
 # Pause to ensure the page loads fully
-pyautogui.sleep(1)
+time.sleep(1)
 
-"Enter Your data Here 👇👇👇"
-students_detail = [
-    ("Registration No","Roll NO"),
-    (2312051573, 6071620)
-     ] 
+#"Enter Your data Here 👇👇👇"
+#Data format (registration,Roll No)
+student_details = [(2312051612, 6071438), (2312051613, 6071439), (2312051507, 6071440), (2312051461, 6071441), (2312051801, 6071442), (2312051538, 6071568), (2312051455, 6071570), (2312051517, 6071571)]
 
 # This data 👆👆 is just an example enter your data properly or take Refferance view of the requirement.txt file which is also in this Repo.
 
-for Registration_No,Roll_no in students_detail:
+for Registration_No,Roll_no in student_details:
     try:
 
         # Locate the Registration No and Roll No fields and enter the data
-        driver.find_element(By.ID, "txtRegistrationNo").send_keys(Registration_No)  # Replace with your Registration No
-        driver.find_element(By.ID, "txtRollNo").send_keys(Roll_no)  # Replace with your Roll No
+        driver.find_element(By.ID, "txtRegistrationNo").send_keys(Registration_No)  # Registration Number one by one
+        driver.find_element(By.ID, "txtRollNo").send_keys(Roll_no)  # Roll Number one by one
 
         # Submit the form
         driver.find_element(By.ID, "cmdbtnProceed").click()
 
         # Pause to let the results load
-        pyautogui.sleep(3)
+        time.sleep(3)
         driver.find_element(By.ID,"imgComfirm").click()
-        pyautogui.sleep(4)
-
-        view_link = driver.find_element(By.ID, "rptMain_ctl01_lnkView")  # Locate the element view and click on it
+        time.sleep(4)
+ # Locate the element view and click on it
+        view_link = driver.find_element(By.ID, "rptMain_ctl01_lnkView") 
         view_link.click()
 
-        pyautogui.sleep(5)
+        time.sleep(5)
 
-      
+#Extracting all the required Details from the result
         student_name = driver.find_element(By.ID,"lblStudentName").text
-        pyautogui.sleep(1)
-
-        "Getting all data from the result window"
+        time.sleep(1)
         father_name = driver.find_element(By.ID,"lblFatherName").text
-        # os_marks = driver.find_element(By.XPATH,"//table//tr[2]//td[8]").text   "Used for getting the data from the result Table"
+        #Extracting Subjects Marks 
+        os_marks = driver.find_element(By.XPATH,"//table//tr[2]//td[8]").text  # "Used for getting the data from the result Table"
   
-        "Finding Total Marks"
+#        "Finding Total Marks"
         total_marks = driver.find_element(By.ID,"rptMarks_ctl06_lblTotal").text
         result = driver.find_element(By.ID,"lblresult").text
         reappear = ""
@@ -72,23 +69,23 @@ for Registration_No,Roll_no in students_detail:
                 "Re appear ": reappear 
                     }
 
-        student_details.append(data)
+        res_std_data.append(data)
         
-        pyautogui.sleep(1)
+        time.sleep(1)
         driver.refresh()
-        pyautogui.sleep(2)
+        time.sleep(2)
 
     except Exception as e:
         print(f"Error is occured In {Registration_No} and \nError is {e}")
         msg.showerror("process Interupted","Unexpected Error: Please ensure good internet connect or try again")
+        
 
-print(student_details)
+print(res_std_data)
 print()   # Giving Extra Space for better Representation
-print()
 
-"Representing Data in well format"
+#"Representing Data in well format"
 print(f"{'Name':<22}: {'Total Marks':<13} {'Reappear ':^20}")
-for dictionary in student_details:
+for dictionary in res_std_data:
     print(f"{dictionary['Name']:<22}: {dictionary['Total Marks']:<13}| {dictionary['Re appear ']:<20}")
 
 # Display info when process complete
--- Rough_2.py
+++ Rough_2.py
@@ -1,3 +1,72 @@
-a = "2 3 4 3 2 4 2 3 4 2 3 "
-processed_numbers = list(map(int,a.split()))
-print(processed_numbers)
+# Required framework and library
+import time
+from tkinter import messagebox as msg
+#"Trying to automate the task using selenium"
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
+
+
+# Set up the WebDriver (Edge in this example)
+options = Options()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Suppress DevTools message
+driver = webdriver.Edge(options=options)
+
+# Navigate to the result portal
+driver.get("https://result.mdu.ac.in/postexam/result.aspx")
+
+res_std_data = []
+
+# Pause to ensure the page loads fully
+time.sleep(1)
+
+#"Enter Your data Here 👇👇👇"
+#Data format (registration,Roll No)
+student_details = [(2312051470,6071607)]
+
+# This data 👆👆 is just an example enter your data properly or take Refferance view of the requirement.txt file which is also in this Repo.
+
+for Registration_No,Roll_no in student_details:
+    try:
+
+        # Locate the Registration No and Roll No fields and enter the data
+        driver.find_element(By.ID, "txtRegistrationNo").send_keys(Registration_No)  # Registration Number one by one
+        driver.find_element(By.ID, "txtRollNo").send_keys(Roll_no)  # Roll Number one by one
+
+        # Submit the form
+        driver.find_element(By.ID, "cmdbtnProceed").click()
+
+        # Pause to let the results load
+        time.sleep(3)
+        driver.find_element(By.ID,"imgComfirm").click()
+        time.sleep(4)
+ # Locate the element view and click on it
+        view_link = driver.find_element(By.ID, "rptMain_ctl01_lnkView") 
+        view_link.click()
+
+        time.sleep(5)
+
+#Extracting all the required Details from the result
+        student_name = driver.find_element(By.ID,"lblStudentName").text
+        time.sleep(1)
+        father_name = driver.find_element(By.ID,"lblFatherName").text
+        #Extracting Subjects Marks 
+        bca201 = driver.find_element(By.XPATH,"//table//tr[2]//td[8]").text
+        bca202 = driver.find_element(By.XPATH,"//table//tr[3]//td[8]").text
+        bca203 = driver.find_element(By.XPATH,"//table//tr[4]//td[8]").text
+        bca204 = driver.find_element(By.XPATH,"//table//tr[5]//td[8]").text
+        bca205 = driver.find_element(By.XPATH,"//table//tr[6]//td[8]").text
+        #Result
+        total_marks = driver.find_element(By.ID,"rptMarks_ctl06_lblTotal").text
+        result = driver.find_element(By.ID,"lblresult").text
+        
+        print(f"{bca201 = }")
+        print(f"{bca202 = }")
+        print(f"{bca203 = }")
+        print(f"{bca204 = }")
+        print(f"{bca205 = }")
+        print(f"{total_marks = }")
+        print(f"{result = }")
+
+    except:
+        pass
